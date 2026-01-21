e37c2c7951b6f26cc5d3490afbc1ad59f8c59fdd
Marius Eriksen <marius.eriksen@nav.no>
2026-01-21 12:47:44 +0100

    Deploy skallet av appen
    Co-authored-by: Nils Martin Sande <nils.martin.sande@nav.no>
    Co-authored-by: Thomas Johansen <thomas.johansen3@nav.no>

---
+++ app/avvist-til-oppgave/README.md
@@ -0,0 +1,9 @@
+## Avvist til oppgave
+
+### TODO:
+1. Få opp skallet og deploy til dev
+2. Lytt på avvist hendelser fra hendelselogg
+3. Sjekk om det finnes en oppgaveId for arbeidssøkerId
+4. Gjør et kall mot oppgave-api for å sjekke status på oppgave
+5. Opprett oppgave i oppgave-api når avvist hendelse mottas
+6. Lagre oppgaveId i db
---
+++ app/avvist-til-oppgave/src/main.rs
@@ -0,0 +1,40 @@
+use std::sync::Arc;
+use health::simple_app_state::AppState;
+use log::LevelFilter;
+use log4rs::Config;
+use log4rs::append::console::ConsoleAppender;
+use log4rs::config::{Appender, Logger, Root};
+use log4rs::encode::json::JsonEncoder;
+use tokio::task::JoinHandle;
+use axum_health::routes;
+
+#[tokio::main]
+async fn main() {
+    init_log();
+    log::info!("Application started");
+    let appstate = Arc::new(AppState::new());
+    let health_routes = routes(appstate);
+    let web_server_task : JoinHandle<Result<(), Box<dyn std::error::Error + Send + Sync>>> = tokio::spawn(async move {
+        let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await?;
+        axum::serve(listener, health_routes).await?;
+        Ok(())
+    });
+
+    match web_server_task.await {
+        Ok(Ok(())) => log::info!("Web server exited successfully."),
+        Ok(Err(e)) => log::error!("Web server error: {}", e),
+        Err(e) => log::error!("Task join error: {}", e),
+    }
+}
+
+fn init_log() {
+    let stdout = ConsoleAppender::builder()
+        .encoder(Box::new(JsonEncoder::new()))
+        .build();
+    let config = Config::builder()
+        .appender(Appender::builder().build("stdout", Box::new(stdout)))
+        .logger(Logger::builder().build("app::rust-nais-api-test", LevelFilter::Info))
+        .build(Root::builder().appender("stdout").build(LevelFilter::Info))
+        .unwrap();
+    log4rs::init_config(config).unwrap();
+}
