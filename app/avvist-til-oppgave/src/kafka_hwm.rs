b8ecb6df68dd09507f80dae3223f119c0b884151
Marius Eriksen <marius.eriksen@nav.no>
2026-01-21 14:56:41 +0100

    Implementer rebalance-listener og annet hwm snacks
    Co-authored-by: Nils Martin Sande <nils.martin.sande@nav.no>
    Co-authored-by: Thomas Johansen <thomas.johansen3@nav.no>

--- app/avvist-til-oppgave/src/kafka_hwm.rs
+++ app/avvist-til-oppgave/src/kafka_hwm.rs
@@ -1,5 +1,8 @@
+use std::error;
 use health::simple_app_state::AppState;
-use rdkafka::Offset;
+use log::error as log_error;
+use rdkafka::consumer::{BaseConsumer, Consumer, ConsumerContext, Rebalance};
+use rdkafka::{ClientContext, Offset};
 use sqlx::{PgPool, Postgres, Transaction};
 use std::sync::Arc;
 
@@ -19,20 +22,63 @@ struct HwmRebalanceHandler {
     pub app_state: Arc<AppState>,
 }
 
-pub async fn get_hwm(tx: &mut Transaction<'_, Postgres>, topic: &str, partition: i32) -> Result<Option<i64>, Box<dyn std::error::Error>> {
+pub async fn get_hwm(
+    tx: &mut Transaction<'_, Postgres>,
+    topic: &str,
+    partition: i32,
+) -> Result<Option<i64>, Box<dyn error::Error>> {
     let row = sqlx::query_scalar(
         r#"
         SELECT offset
         FROM kafka_hwms
         WHERE topic = $1 AND partition = $2
         "#,
-    ).bind(topic).bind(partition).fetch_optional(&mut **tx).await?;
+    )
+        .bind(topic)
+        .bind(partition)
+        .fetch_optional(&mut **tx)
+        .await?;
 
     Ok(row)
 }
 
+impl ClientContext for HwmRebalanceHandler {}
+impl ConsumerContext for HwmRebalanceHandler {
+    fn post_rebalance(&self, base_consumer: &BaseConsumer<Self>, rebalance: &Rebalance<'_>) {
+        match rebalance {
+            Rebalance::Assign(partitions) => {
+                let topic_partitions = partitions.elements().iter().map(|tp| TopicPartition {
+                    topic: tp.topic().to_string(),
+                    partition: tp.partition(),
+                }).collect();
+
+                let hwms = futures::executor::block_on(self.get_hwms(topic_partitions)).unwrap();
+                hwms.iter().for_each(|hwm| {
+                    log::info!("HWM for topic {} partition {} is {}", hwm.topic, hwm.partition, hwm.offset);
+                    base_consumer.seek(
+                        &hwm.topic,
+                        hwm.partition,
+                        hwm.seek_to_rd_kafka_offset(),
+                        std::time::Duration::from_secs(10),
+                    ).unwrap();
+                })
+            }
+            Rebalance::Revoke(partitions) => {
+                log::info!("Partitions revoked: {:?}", partitions);
+            }
+            Rebalance::Error(kafka_error) => {
+                log_error!("Rebalance error: {}", kafka_error);
+                self.app_state.set_is_alive(false)
+            }
+        }
+    }
+}
+
 impl HwmRebalanceHandler {
-    async fn get_hwms(&self, topic_partitions: Vec<TopicPartition>) -> Result<Vec<Hwm>, Box<dyn std::error::Error>> {
+    async fn get_hwms(
+        &self,
+        topic_partitions: Vec<TopicPartition>,
+    ) -> Result<Vec<Hwm>, Box<dyn std::error::Error>> {
         let mut tx = self.pg_pool.begin().await?;
         let mut hwms = Vec::new();
         for tp in topic_partitions {
