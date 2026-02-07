a79f23b9b6b1215436f367b15a25ece4f49fb1ef
Marius Eriksen <marius.eriksen@nav.no>
2026-02-07 10:10:38 +0100

    Endre struktur og navn på tabeller etter diskusjon om hva vi behøver
    Lag en v2 av databasen så vi ikke får krøll med endringer i migreringsscript og slik at vi starter med blanke ark
    
    Co-authored-by: Nils Martin Sande <nils.martin.sande@nav.no>
    Co-authored-by: Thomas Johansen <thomas.johansen3@nav.no>

---
+++ app/avvist-til-oppgave/migrations/20260204120408_oppgaver.sql
@@ -0,0 +1,19 @@
+CREATE TABLE oppgaver
+(
+    id               BIGSERIAL PRIMARY KEY,
+    type             VARCHAR(50) NOT NULL,
+    melding_id       UUID        NOT NULL,
+    opplysninger     VARCHAR(50)[] NOT NULL,
+    arbeidssoeker_id BIGINT      NOT NULL,
+    identitetsnummer VARCHAR(11) NOT NULL,
+    tidspunkt        TIMESTAMP   NOT NULL
+);
+
+CREATE TABLE oppgave_status_logg
+(
+    id         BIGSERIAL PRIMARY KEY,
+    oppgave_id BIGINT REFERENCES oppgaver (id) ON DELETE CASCADE,
+    status     VARCHAR(50) NOT NULL,
+    melding    TEXT        NOT NULL,
+    tidspunkt  TIMESTAMP   NOT NULL
+);
