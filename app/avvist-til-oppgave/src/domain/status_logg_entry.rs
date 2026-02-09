55d57e015eb43593646778428c9503d7e645494c
Marius Eriksen <marius.eriksen@nav.no>
2026-02-09 16:09:07 +0100

    La status være String til vi vet litt mer hva det er

--- app/avvist-til-oppgave/src/domain/status_logg_entry.rs
+++ app/avvist-til-oppgave/src/domain/status_logg_entry.rs
@@ -1,14 +1,13 @@
-use crate::domain::oppgave_status::OppgaveStatus;
 use chrono::{DateTime, Utc};
 
 #[derive(Debug, Clone, PartialEq)]
 pub struct StatusLoggEntry {
-    pub status: OppgaveStatus,
+    pub status: String,
     pub tidspunkt: DateTime<Utc>,
 }
 
 impl StatusLoggEntry {
-    pub fn new(status: OppgaveStatus, tidspunkt: DateTime<Utc>) -> Self {
+    pub fn new(status: String, tidspunkt: DateTime<Utc>) -> Self {
         Self { status, tidspunkt }
     }
 }
