a40b8ef8f70e259731d6d46c036b6ee46d673427
Nils Martin Sande <nils.martin.sande@nav.no>
2026-02-19 20:57:02 +0100

    Fisket datatype for timestamp

--- app/utgang/migrations/20260212060007_data_tables.sql
+++ app/utgang/migrations/20260212060007_data_tables.sql
@@ -5,7 +5,7 @@ create table periode (
     periode_startet_brukertype  VARCHAR not null,
     periode_stoppet_timestamp   TIMESTAMPZ(3) default null,
     periode_stoppet_brukertype  VARCHAR default null,
-    sist_oppdatert_timestamp    TIMESTAMPZ(3) not null,
+    sist_oppdatert_timestamp    TIMESTAMP(3) not null,
     sist_oppdaterte_status      VARCHAR not null
 );
 
@@ -20,6 +20,6 @@ create table opplysninger (
     id              BIGSERIAL PRIMARY KEY,
     periode_id      UUID NOT NULL REFERENCES periode_metdata(id) ON DELETE CASCADE,
     kilde           VARCHAR NOT NULL,
-    tidspunkt       TIMESTAMPZ(3) NOT NULL,
+    tidspunkt       TIMESTAMP(3) NOT NULL,
     opplysninger    VARCHAR[] NOT NULL
 );
