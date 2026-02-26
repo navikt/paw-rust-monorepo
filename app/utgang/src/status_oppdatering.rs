eb8d2433e247630e5a0af433bf44c62a034ddf59
Nils Martin Sande <nils.martin.sande@nav.no>
2026-02-26 14:41:37 +0100

    La til struct for status oppdatering

--- app/utgang/src/status_oppdatering.rs
+++ app/utgang/src/status_oppdatering.rs
@@ -1,7 +1,29 @@
+use std::{num::NonZeroU16, ops::Deref, sync::Arc};
+
 use crate::pdl::pdl_query::PDLClient;
 use sqlx::PgPool;
 
+#[derive(Clone)]
 pub struct StatusOppdatering {
+    inner: Arc<StatusOppdateringRef>,
+}
+
+struct StatusOppdateringRef {
     pg_pool: PgPool,
     pdl_client: PDLClient,
+    batch_size: NonZeroU16,
+}
+
+impl StatusOppdatering {
+    pub fn new(pg_pool: PgPool, pdl_client: PDLClient, batch_size: NonZeroU16) -> Self {
+        Self {
+            inner: Arc::new(StatusOppdateringRef {
+                pg_pool,
+                pdl_client,
+                batch_size,
+            }),
+        }
+    }
+
+    pub fn kjoer_oppdatering() {}
 }
