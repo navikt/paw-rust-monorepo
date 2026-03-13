71a71b5a63508a51ea64e79b24cc8bbe087c9223
Marius Eriksen <marius.eriksen@nav.no>
2026-03-13 12:03:54 +0100

    Legg til funksjonalitet for fastsetting av runtime environment

--- lib/paw_rust_base/src/env.rs
+++ lib/paw_rust_base/src/env.rs
@@ -14,6 +14,63 @@ pub fn nais_namespace() -> Result<String> {
     get_env("NAIS_NAMESPACE")
 }
 
+pub const NAIS_CLUSTER_NAME: &str = "NAIS_CLUSTER_NAME";
 pub fn nais_cluster_name() -> Result<String> {
-    get_env("NAIS_CLUSTER_NAME")
+    get_env(NAIS_CLUSTER_NAME)
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum RuntimeEnv {
+    ProdGcp,
+    DevGcp,
+    Local,
+    UnknownEnv(String),
+}
+
+pub const PROD_GCP_CLUSTER_NAME: &str = "prod-gcp";
+pub const DEV_GCP_CLUSTER_NAME: &str = "dev-gcp";
+
+pub fn runtime_env() -> RuntimeEnv {
+    match nais_cluster_name() {
+        Ok(cluster) if cluster == PROD_GCP_CLUSTER_NAME => RuntimeEnv::ProdGcp,
+        Ok(cluster) if cluster == DEV_GCP_CLUSTER_NAME => RuntimeEnv::DevGcp,
+        Ok(cluster) => RuntimeEnv::UnknownEnv(cluster),
+        Err(_) => RuntimeEnv::Local,
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn returnerer_prod_nar_cluster_er_prod_gcp() {
+        temp_env::with_var(NAIS_CLUSTER_NAME, Some(PROD_GCP_CLUSTER_NAME), || {
+            assert_eq!(runtime_env(), RuntimeEnv::ProdGcp);
+        });
+    }
+
+    #[test]
+    fn returnerer_dev_nar_cluster_er_dev_gcp() {
+        temp_env::with_var(NAIS_CLUSTER_NAME, Some(DEV_GCP_CLUSTER_NAME), || {
+            assert_eq!(runtime_env(), RuntimeEnv::DevGcp);
+        });
+    }
+
+    #[test]
+    fn returnerer_local_nar_cluster_mangler() {
+        temp_env::with_var_unset(NAIS_CLUSTER_NAME, || {
+            assert_eq!(runtime_env(), RuntimeEnv::Local);
+        });
+    }
+
+    #[test]
+    fn returnerer_unknown_nais_nar_cluster_er_ukjent() {
+        temp_env::with_var(NAIS_CLUSTER_NAME, Some("prod-fss"), || {
+            assert_eq!(
+                runtime_env(),
+                RuntimeEnv::UnknownEnv("prod-fss".to_string())
+            );
+        });
+    }
 }
