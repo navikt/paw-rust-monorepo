ba23cca9cfec4cf51692a5c62ad4a35b09dbcb23
Marius Eriksen <marius.eriksen@nav.no>
2026-03-31 10:34:33 +0200

    Fjern azure_m2m_client da texas client brukes istedenfor

--- lib/azure_m2m_client/src/lib.rs
+++
@@ -1,8 +0,0 @@
-pub mod client;
-pub mod config;
-pub mod error;
-pub mod response;
-
-pub use client::{AzureAdM2MClient, M2MTokenClient};
-pub use config::AzureAdM2MConfig;
-pub use error::AzureAdM2MClientError;
