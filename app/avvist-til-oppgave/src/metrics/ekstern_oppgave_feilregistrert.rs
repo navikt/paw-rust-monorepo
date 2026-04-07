33d986fea57ce7063306e3c71b8498e6e7ff336a
Marius Eriksen <marius.eriksen@nav.no>
2026-04-07 11:54:28 +0200

    Metrikk for antall oppgaver markert som feilregistrert

---
+++ app/avvist-til-oppgave/src/metrics/ekstern_oppgave_feilregistrert.rs
@@ -0,0 +1,30 @@
+use crate::domain::hendelse_logg_status::HendelseLoggStatus::EksternOppgaveFeilregistrert;
+use anyhow::Result;
+use prometheus::{register_gauge, Gauge};
+use sqlx::{Postgres, Transaction};
+use std::sync::LazyLock;
+
+static EKSTERN_OPPGAVE_FEILREGISTRERT: LazyLock<Gauge> = LazyLock::new(|| {
+    register_gauge!(
+        "avvist_til_oppgave_ekstern_oppgave_feilregistrert_total",
+        "Totalt antall oppgaver som har blitt feilregistrert i eksternt Oppgave API"
+    )
+    .expect("Failed to register avvist_til_oppgave_ekstern_oppgave_feilregistrert_total gauge")
+});
+
+pub async fn oppdater(transaction: &mut Transaction<'_, Postgres>) -> Result<()> {
+    let antall: i64 = sqlx::query_scalar(
+        //language=PostgreSQL
+        r#"
+        SELECT COUNT(*)
+        FROM oppgave_hendelse_logg
+        WHERE status = $1
+        "#,
+    )
+    .bind(EksternOppgaveFeilregistrert.to_string())
+    .fetch_one(&mut **transaction)
+    .await?;
+
+    EKSTERN_OPPGAVE_FEILREGISTRERT.set(antall as f64);
+    Ok(())
+}
