b1288aed6443afbfe4bf48ee7c8aa08848257e4d
Nils Martin Sande <nils.martin.sande@nav.no>
2026-04-30 11:53:29 +0200

    La til konvertering fra Startet til periode-rad

--- app/utgang/src/dao/converters/from_startet.rs
+++ app/utgang/src/dao/converters/from_startet.rs
@@ -1,10 +1,13 @@
 use interne_hendelser::Startet;
 
 use crate::{
-    dao::utgang_hendelse::{Input, InternUtgangHendelse},
+    dao::{
+        perioder::PeriodeRad,
+        utgang_hendelse::{Input, InternUtgangHendelse},
+    },
     domain::{
-        Opplysninger::Opplysninger, arbeidssoekerperiode_id::ArbeidssoekerperiodeId,
-        utgang_hendelse_type::UtgangHendelseType,
+        Opplysninger::Opplysninger, arbeidssoeker_id::ArbeidssoekerId,
+        arbeidssoekerperiode_id::ArbeidssoekerperiodeId, utgang_hendelse_type::UtgangHendelseType,
     },
 };
 
@@ -17,5 +20,17 @@ impl From<Startet> for InternUtgangHendelse<Input> {
             h.metadata.utfoert_av.bruker_type,
             Some(Opplysninger(h.opplysninger)),
         )
+    }
+}
+
+impl From<&Startet> for PeriodeRad {
+    fn from(value: &Startet) -> Self {
+        PeriodeRad {
+            id: value.hendelse_id.into(),
+            arbeidssoeker_id: Some(ArbeidssoekerId(value.id)),
+            trenger_kontroll: false,
+            stoppet: false,
+            sist_oppdatert: value.metadata.tidspunkt,
+        }
     }
 }
