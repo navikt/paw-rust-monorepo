1ecf300f817c367bd25cf04cda3bbae2c0eac9f2
Nils Martin Sande <nils.martin.sande@nav.no>
2026-05-07 07:24:36 +0200

    La til funksjon for å utlede fakta for en liste med personer

--- domain/regler_arbeidssoeker/src/fakta/mod.rs
+++ domain/regler_arbeidssoeker/src/fakta/mod.rs
@@ -1,12 +1,23 @@
-mod config;
-pub mod person_fakta;
-mod alder_fakta;
 mod adresse_fakta;
+mod alder_fakta;
+mod config;
 mod folkeregister_fakta;
-mod statsborgerskap_fakta;
 mod oppholdstillatelse_fakta;
+pub mod person_fakta;
+mod statsborgerskap_fakta;
 mod utflytting_fakta;
 
 pub trait UtledeFakta<INN, UT> {
     fn utlede_fakta(&self, input: &INN) -> anyhow::Result<Vec<UT>>;
+
+    fn utlede_fakta_liste<'a, K>(
+        &self,
+        input: &'a [(K, INN)],
+    ) -> Vec<(&'a K, anyhow::Result<Vec<UT>>)> {
+        input
+            .iter()
+            .map(|(key, value)| (key, self.utlede_fakta(value)))
+            .collect()
+    }
 }
+
