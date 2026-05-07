450c3af952b491ee135f36e6d3bde38c32b4198a
Marius Eriksen <marius.eriksen@nav.no>
2026-05-07 12:25:22 +0200

    Legg til i64 -> ArbeidssøkerId, samt Display implementasjon

--- domain/types/src/arbeidssoeker_id.rs
+++ domain/types/src/arbeidssoeker_id.rs
@@ -1,7 +1,23 @@
+use fmt::Display;
+use std::fmt;
+
+#[derive(Debug, Clone, Copy, PartialEq)]
 pub struct ArbeidssoekerId(pub i64);
 
+impl From<i64> for ArbeidssoekerId {
+    fn from(id: i64) -> Self {
+        ArbeidssoekerId(id)
+    }
+}
+
 impl From<ArbeidssoekerId> for i64 {
-    fn from(arbeidssoeker_id: ArbeidssoekerId) -> Self {
-        arbeidssoeker_id.0
+    fn from(id: ArbeidssoekerId) -> Self {
+        id.0
+    }
+}
+
+impl Display for ArbeidssoekerId {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.0)
     }
 }
