acd5b2d724cc04f72a3d3386de664f45aa47e373
Marius Eriksen <marius.eriksen@nav.no>
2026-05-27 15:39:23 +0200

    veileder-oppgave: Bruk rebalance istedenfor post_rebalance

--- app/veileder-oppgave/src/kafka/consumer.rs
+++ app/veileder-oppgave/src/kafka/consumer.rs
@@ -1,7 +1,7 @@
 use anyhow::Result;
 use health_and_monitoring::simple_app_state::AppState;
 use paw_rdkafka::kafka_config::KafkaConfig;
-use paw_rdkafka_hwm::hwm_rebalance_handler::HwmRebalanceHandler;
+use paw_rdkafka_hwm::rebalance::rebalance_handler::RebalanceHandler;
 use rdkafka::consumer::{Consumer, StreamConsumer};
 use sqlx::PgPool;
 use std::sync::Arc;
@@ -11,15 +11,15 @@ pub fn create(
     pg_pool: PgPool,
     kafka_config: KafkaConfig,
     topics: &[&str],
-) -> Result<StreamConsumer<HwmRebalanceHandler>> {
+) -> Result<StreamConsumer<RebalanceHandler>> {
     let hwm_version = *kafka_config.hwm_version;
     let config = kafka_config.rdkafka_client_config()?;
-    let context = HwmRebalanceHandler {
+    let context = RebalanceHandler {
         pg_pool,
         app_state,
         version: hwm_version,
     };
-    let consumer: StreamConsumer<HwmRebalanceHandler> = config.create_with_context(context)?;
+    let consumer: StreamConsumer<RebalanceHandler> = config.create_with_context(context)?;
     consumer.subscribe(topics)?;
     Ok(consumer)
 }
