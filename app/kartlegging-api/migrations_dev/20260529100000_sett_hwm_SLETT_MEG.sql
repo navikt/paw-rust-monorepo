d6908d1859ea0fa10bf05a6c5d1860f256c81587
Thomas Johansen <thomas.johansen3@nav.no>
2026-07-16 13:42:01 +0200

    Flyttet dev HWMs

--- app/kartlegging-api/migrations_dev/20260529100000_sett_hwm_SLETT_MEG.sql
+++ app/kartlegging-api/migrations_dev/20260529100000_sett_hwm_SLETT_MEG.sql
@@ -1,45 +1,45 @@
 INSERT INTO hwm (version, topic, partition, hwm)
-VALUES (1, 'paw.arbeidssokerperioder-v1', 0, 5292),               -- 5340 - a685481e-238f-4d61-b3bd-c7a149836fe9
-       (1, 'paw.arbeidssokerperioder-v1', 1, 5061),               -- 5113 - 9704e73d-d38e-4935-899c-d293abf0e338
-       (1, 'paw.arbeidssokerperioder-v1', 2, 4956),               -- 4966 - 1fe6070d-e50e-4dfc-b14f-69d25737d42a
-       (1, 'paw.arbeidssokerperioder-v1', 3, 4499),               -- 4694 - 5aebe375-d496-42e3-bcac-0beb8e32ba88
-       (1, 'paw.arbeidssokerperioder-v1', 4, 4838),               -- 4878 - bbff32a8-4831-4022-a135-f26b8d054ff4
-       (1, 'paw.arbeidssokerperioder-v1', 5, 5268),               -- 5278 - 42d20c57-2321-4dd6-803f-68c6bd993490
-       (1, 'paw.opplysninger-om-arbeidssoeker-v1', 0, 4323),      -- 4333 - c4a027cf-6dd6-4870-9dea-c05cfb89a199
-       (1, 'paw.opplysninger-om-arbeidssoeker-v1', 1, 3907),      -- 3917 - ddfcb7fd-7ba0-45c4-9eba-fe145ea934a7
-       (1, 'paw.opplysninger-om-arbeidssoeker-v1', 2, 4059),      -- 4069 - 4d90e9eb-f226-4b6f-a3dc-0d00294f389a
-       (1, 'paw.opplysninger-om-arbeidssoeker-v1', 3, 3837),      -- 3847 - 2eb7936b-1ec2-451d-a989-9c2a4ca5e16c
-       (1, 'paw.opplysninger-om-arbeidssoeker-v1', 4, 3875),      -- 3885 - 863bbac6-7fe2-48f4-a9a3-b8569b1455ab
-       (1, 'paw.opplysninger-om-arbeidssoeker-v1', 5, 4141),      -- 4151 - 7678b050-f810-4e1d-8a3d-fe9e1571b59a
-       (1, 'paw.arbeidssoker-profilering-v1', 0, 4342),           -- 4352 - b0621032-4c76-4de2-8eeb-7fec0eef4270
-       (1, 'paw.arbeidssoker-profilering-v1', 1, 3897),           -- 3907 - 6f8d84e8-5a4b-4096-a0bf-2c2bd368a492
-       (1, 'paw.arbeidssoker-profilering-v1', 2, 4087),           -- 4097 - 9a9e3d93-258f-4918-b8bb-c0844618a69a
-       (1, 'paw.arbeidssoker-profilering-v1', 3, 3860),           -- 3870 - 750cc34f-a2cb-4ed2-95ad-f8d89ab0d1ec
-       (1, 'paw.arbeidssoker-profilering-v1', 4, 3871),           -- 3881 - cce9c860-d0bb-4b8c-a404-af77e3c57d40
-       (1, 'paw.arbeidssoker-profilering-v1', 5, 4152),           -- 4162 - 00b4d7fd-a2e0-4b53-b07a-5fb12e409b27
-       (1, 'paw.arbeidssoeker-egenvurdering-v1', 0, 18),          -- 18
-       (1, 'paw.arbeidssoeker-egenvurdering-v1', 1, 22),          -- 22
-       (1, 'paw.arbeidssoeker-egenvurdering-v1', 2, 11),          -- 11
-       (1, 'paw.arbeidssoeker-egenvurdering-v1', 3, 9),           -- 9
-       (1, 'paw.arbeidssoeker-egenvurdering-v1', 4, 9),           -- 9
-       (1, 'paw.arbeidssoeker-egenvurdering-v1', 5, 14),          -- 14
-       (1, 'paw.arbeidssoker-bekreftelse-v1', 0, 186),            -- 186
-       (1, 'paw.arbeidssoker-bekreftelse-v1', 1, 74),             -- 74
-       (1, 'paw.arbeidssoker-bekreftelse-v1', 2, 107),            -- 107
-       (1, 'paw.arbeidssoker-bekreftelse-v1', 3, 158),            -- 158
-       (1, 'paw.arbeidssoker-bekreftelse-v1', 4, 193),            -- 202
-       (1, 'paw.arbeidssoker-bekreftelse-v1', 5, 133),            -- 133
-       (1, 'paw.arbeidssoker-bekreftelse-paavegneav-v1', 0, 458), -- 464
-       (1, 'paw.arbeidssoker-bekreftelse-paavegneav-v1', 1, 362), -- 369
-       (1, 'paw.arbeidssoker-bekreftelse-paavegneav-v1', 2, 386), -- 386
-       (1, 'paw.arbeidssoker-bekreftelse-paavegneav-v1', 3, 498), -- 533
-       (1, 'paw.arbeidssoker-bekreftelse-paavegneav-v1', 4, 412), -- 418
-       (1, 'paw.arbeidssoker-bekreftelse-paavegneav-v1', 5, 458), -- 458
-       (1, 'poao.siste-oppfolgingsperiode-v3', 0, 8403),          -- 9403
-       (1, 'poao.siste-oppfolgingsperiode-v3', 1, 8224),          -- 9224
-       (1, 'poao.siste-oppfolgingsperiode-v3', 2, 8069),          -- 9069
-       (1, 'poao.siste-oppfolgingsperiode-v3', 3, 8321),          -- 9321
-       (1, 'poao.siste-oppfolgingsperiode-v3', 4, 8148),          -- 9148
-       (1, 'poao.siste-oppfolgingsperiode-v3', 5, 8105),          -- 9105
-       (1, 'poao.siste-oppfolgingsperiode-v3', 6, 8202),          -- 9202
-       (1, 'poao.siste-oppfolgingsperiode-v3', 7, 8210);          -- 9210
+VALUES (1, 'paw.arbeidssokerperioder-v1', 0, 5240),             -- 5340
+       (1, 'paw.arbeidssokerperioder-v1', 1, 5013),             -- 5113
+       (1, 'paw.arbeidssokerperioder-v1', 2, 4866),             -- 4966
+       (1, 'paw.arbeidssokerperioder-v1', 3, 4594),             -- 4694
+       (1, 'paw.arbeidssokerperioder-v1', 4, 4778),             -- 4878
+       (1, 'paw.arbeidssokerperioder-v1', 5, 5178),             -- 5278
+       (1, 'paw.opplysninger-om-arbeidssoeker-v1', 0, 4233),    -- 4333
+       (1, 'paw.opplysninger-om-arbeidssoeker-v1', 1, 3817),    -- 3917
+       (1, 'paw.opplysninger-om-arbeidssoeker-v1', 2, 3969),    -- 4069
+       (1, 'paw.opplysninger-om-arbeidssoeker-v1', 3, 3747),    -- 3847
+       (1, 'paw.opplysninger-om-arbeidssoeker-v1', 4, 3785),    -- 3885
+       (1, 'paw.opplysninger-om-arbeidssoeker-v1', 5, 4051),    -- 4151
+       (1, 'paw.arbeidssoker-profilering-v1', 0, 4252),         -- 4352
+       (1, 'paw.arbeidssoker-profilering-v1', 1, 3807),         -- 3907
+       (1, 'paw.arbeidssoker-profilering-v1', 2, 3997),         -- 4097
+       (1, 'paw.arbeidssoker-profilering-v1', 3, 3770),         -- 3870
+       (1, 'paw.arbeidssoker-profilering-v1', 4, 3781),         -- 3881
+       (1, 'paw.arbeidssoker-profilering-v1', 5, 4062),         -- 4162
+       (1, 'paw.arbeidssoeker-egenvurdering-v1', 0, 0),         -- 18
+       (1, 'paw.arbeidssoeker-egenvurdering-v1', 1, 0),         -- 22
+       (1, 'paw.arbeidssoeker-egenvurdering-v1', 2, 0),         -- 11
+       (1, 'paw.arbeidssoeker-egenvurdering-v1', 3, 0),         -- 9
+       (1, 'paw.arbeidssoeker-egenvurdering-v1', 4, 0),         -- 9
+       (1, 'paw.arbeidssoeker-egenvurdering-v1', 5, 0),         -- 14
+       (1, 'paw.arbeidssoker-bekreftelse-v1', 0, 0),            -- 186
+       (1, 'paw.arbeidssoker-bekreftelse-v1', 1, 0),            -- 74
+       (1, 'paw.arbeidssoker-bekreftelse-v1', 2, 0),            -- 107
+       (1, 'paw.arbeidssoker-bekreftelse-v1', 3, 0),            -- 158
+       (1, 'paw.arbeidssoker-bekreftelse-v1', 4, 0),            -- 202
+       (1, 'paw.arbeidssoker-bekreftelse-v1', 5, 0),            -- 133
+       (1, 'paw.arbeidssoker-bekreftelse-paavegneav-v1', 0, 0), -- 464
+       (1, 'paw.arbeidssoker-bekreftelse-paavegneav-v1', 1, 0), -- 369
+       (1, 'paw.arbeidssoker-bekreftelse-paavegneav-v1', 2, 0), -- 386
+       (1, 'paw.arbeidssoker-bekreftelse-paavegneav-v1', 3, 0), -- 533
+       (1, 'paw.arbeidssoker-bekreftelse-paavegneav-v1', 4, 0), -- 418
+       (1, 'paw.arbeidssoker-bekreftelse-paavegneav-v1', 5, 0), -- 458
+       (1, 'poao.siste-oppfolgingsperiode-v3', 0, 8403),        -- 9403
+       (1, 'poao.siste-oppfolgingsperiode-v3', 1, 8224),        -- 9224
+       (1, 'poao.siste-oppfolgingsperiode-v3', 2, 8069),        -- 9069
+       (1, 'poao.siste-oppfolgingsperiode-v3', 3, 8321),        -- 9321
+       (1, 'poao.siste-oppfolgingsperiode-v3', 4, 8148),        -- 9148
+       (1, 'poao.siste-oppfolgingsperiode-v3', 5, 8105),        -- 9105
+       (1, 'poao.siste-oppfolgingsperiode-v3', 6, 8202),        -- 9202
+       (1, 'poao.siste-oppfolgingsperiode-v3', 7, 8210); -- 9210
