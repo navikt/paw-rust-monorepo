5402465ad51650875c0554d27bf7ca33383a5f92
Thomas Johansen <thomas.johansen3@nav.no>
2026-07-22 21:08:48 +0200

    Fjernet ubrukte rammeverk

--- domain/dab_kontortilhorlighet/src/lib.rs
+++
@@ -1,8 +0,0 @@
-use graphql_client::GraphQLQuery;
-
-#[derive(GraphQLQuery)]
-#[graphql(
-    schema_path = "graphql/dab-schema.graphql",
-    query_path = "graphql/hentKontortilhorligheter.graphql"
-)]
-pub struct HentKontortilhorligheter;
